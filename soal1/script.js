7418644bb4be748e71f20e2a943aa345e206e08c
rqz234 <160827712+rqz234@users.noreply.github.com>
2024-04-18 14:16:22 +0700

    Update

--- soal1/script.js
+++ soal1/script.js
@@ -1,9 +1,11 @@
 // lanjutkan script ini ya ...
-let nama = document.getElementById("nama", "arif");
+let nama = "Arif";
+let tampilNama = document.getElementById("nama").innerText = nama;
 
 let hobi = ["Baca", "Bermain Game", "Belajar Coding"];
 let listhobi = document.getElementById("listhobi");
 hobi.forEach(showHobi);
+let tampilHobi = document.getElementById("hobi").innerText = hobi[2];
 
 
 function showHobi(value, index)
---
+++ soal2/script.js
@@ -0,0 +1,12 @@
+let npm = 2226240001;
+let nama = "Arif";
+let nilai = [80, 70, 90, 85, 60, 88];
+
+// lanjutkan scriptnya ya ...
+let tampilNpm = document.getElementById("npm").innerText = npm;
+let tampilNama = document.getElementById("nama").innerText = nama;
+let tampilNilai = document.getElementById("nilaimax").innerText = nilai[2];
+
+
+
+
